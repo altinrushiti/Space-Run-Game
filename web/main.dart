301fd9b76d312d618714fcf38400179f18304667
Nane Kratzke <nane.kratzke@th-luebeck.de>
2022-12-15 15:45:25 +0000

    Dev Containerized

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Hello, this was a fast start!';
+  querySelector('#header').innerHtml = '<h1>Hoooray, your first Dart App is running!</h1>';
+  querySelector('#footer').innerHtml += '<p>Now, containerize the world!</>';
 }
